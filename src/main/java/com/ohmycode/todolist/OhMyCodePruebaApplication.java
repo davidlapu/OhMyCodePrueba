42ebe72a7cff5d4556676cbd6699196e5c89696f
David Lama Pulido <davidlamapul@gmail.com>
2022-10-11 17:58:50 +0200

    Initial commit

---
+++ src/main/java/com/ohmycode/todolist/OhMyCodePruebaApplication.java
@@ -0,0 +1,13 @@
+package com.ohmycode.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhMyCodePruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhMyCodePruebaApplication.class, args);
+	}
+
+}
